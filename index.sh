1fbdddcc19164b13a19a0d2856c4c68aa7b3032d
Ryan Teodoro <ryanteodoro@theagilehub.net>
2019-05-04 16:51:06 -0500

    Improve if logic to avoid nesting entire script within else statement

--- index.sh
+++ index.sh
@@ -8,10 +8,9 @@ INFO="[GIT-BRANCH-AUTOCOMPLETE]"
 echo $INFO
 
 # check for .git directory
-if [[ -d .git ]]; then
-  echo "$INFO: Found .git directory"
-else
+if [[ ! -d .git ]]; then
   echo "$INFO: No .git directory found"
+  exit 0
 fi
 
-echo "${CARS[*]}"
+echo "$INFO: Done!"
